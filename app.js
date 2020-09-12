f4f4355a4a2bc22f702e5a3f523209e118da874e
Sam <programming@vectors.net.au>
2020-09-12 21:21:54 +0800

    init

---
+++ app.js
@@ -0,0 +1,21 @@
+const correctAnswers = ['B', 'B', 'B', 'B'];
+const form = document.querySelector('.quiz-form');
+const result = document.querySelector('.result');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    let score = 0;
+    const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
+    // compare userAnswers to correctAnswers
+    userAnswers.forEach((answer, index) => {
+        if (answer === correctAnswers[index]) {
+            score += 25;
+        }
+    });
+
+    // console.log(score);
+    // show result
+    result.querySelector('span').textContent = `${score}%`;
+    result.classList.remove('d-none');
+})
